0a861938d5448365dd0e3437b69ca158b1abf13e
Victor Cristiano <Victorcristianoods@gmail.com>
2022-10-19 16:34:11 -0300

    teste inicial

---
+++ src/data/words.js
@@ -0,0 +1,23 @@
+export const wordsList = {
+  /* Carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  Fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  Corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  Computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  Programação: ["Linguagem", "Framework", "JavaScript", "React"],
+  Alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"], */
+
+  Personagens_da_Biblia: ['Abraao', 'Arao', 'salomao', 'davi', 'golias', 'Ezequiel', 'enoque', 'saul', 'josue', 'daniel', 'sadraque', 'mesaque'],
+
+  Jesus_é: ['caminho', 'verdade','vida', 'salvacao','leao','cordeiro'],
+
+  Discipulos: ["Mateus", "Marcos", "lucas", "joao", "pedro", 'judas', 'andre', 'tiago','filipe','tome','bartolomeu', ],
+  // Louvor: ['Deus-me-Ama', 'Ha-um-caminho', 'Ha-um-rio', ],
+  
+  Livros_da_Biblia: ["Genesis", "exodo", "levitico", "romanos", "mateus", "lucas", 'apocalipse', 'numeros', 'cronicas', 'rute', 'zacarias', 'malaquias' ],
+};
+
+
+
+
+
+
